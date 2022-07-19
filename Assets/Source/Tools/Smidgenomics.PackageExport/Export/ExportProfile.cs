c8994d2c55d73121f65677d48a7730b8d67ed07e
Unnar Krist <unnarvc@speedymail.org>
2022-07-19 11:01:35 +0000

    Fix gitignore including subfolder

---
+++ Assets/Source/Tools/Smidgenomics.PackageExport/Export/ExportProfile.cs
@@ -0,0 +1,112 @@
+﻿// Smidgens @ github
+
+namespace Smidgenomics.Tools.Packages
+{
+	using UnityEngine;
+
+	/// <summary>
+	/// Export configuration
+	/// </summary>
+	[CreateAssetMenu(menuName = CFG.CreateMenu.EXPORT_PROFILE)]
+	internal class ExportProfile : ScriptableObject
+	{
+		public string Package => _package;
+		public CompressionMethod CompressionMode => _compression;
+
+		// helper for inspector
+		public static class FName
+		{
+			public const string PACKAGE_NAME = nameof(_package);
+		}
+
+		[Space(5f)]
+		[PackageName]
+		[Tooltip("Folder name under Packages")]
+		[SerializeField] private string _package = "";
+
+		[SerializeField] private CompressionMethod _compression = CompressionMethod.Zip;
+	}
+}
+
+namespace Smidgenomics.Tools.Packages
+{
+	using System;
+	using UnityEngine;
+
+	internal static class ExportProfile_Extensions
+	{
+		/// <summary>
+		/// Export from preset
+		/// </summary>
+		public static void Export(this ExportProfile preset)
+		{
+			try
+			{
+				var exportFolder = CFG.ProjectPath.PACKAGE_EXPORTS;
+				var package = PackageFolder.Load(preset.Package);
+				package?.Export(preset.CompressionMode, exportFolder);
+			}
+			catch (Exception e)
+			{
+				Debug.LogError("Unknown error exporting package.");
+				Debug.LogError(e.Message);
+			}
+		}
+	}
+}
+
+
+namespace Smidgenomics.Tools.Packages
+{
+	using UnityEngine;
+	using UnityEditor;
+	using System.IO;
+
+	[CustomEditor(typeof(ExportProfile))]
+	internal class ExportProfile_Editor : Editor
+	{
+		public override void OnInspectorGUI()
+		{
+			serializedObject.UpdateIfRequiredOrScript();
+			base.OnInspectorGUI();
+			serializedObject.ApplyModifiedProperties();
+			GUILayout.Space(5f);
+			DrawInfos();
+		}
+
+		private SerializedProperty _package = null;
+
+
+		private void OnEnable()
+		{
+			_package = serializedObject.FindProperty(ExportProfile.FName.PACKAGE_NAME);
+		}
+
+		private void DrawInfos()
+		{
+			if(_package.stringValue.Length == 0) { return; }
+
+			var proot = CFG.ProjectPath.PACKAGES;
+			var fpath = $"{proot}/{_package.stringValue}";
+
+			if(!Directory.Exists(fpath))
+			{
+				EditorGUILayout.HelpBox($"Package folder '{_package.stringValue}' missing", MessageType.Error);
+				return;
+			}
+
+			using (new GUILayout.HorizontalScope(EditorStyles.helpBox))
+			{
+				GUILayout.Label(fpath, EditorStyles.wordWrappedMiniLabel);
+
+				GUILayout.FlexibleSpace();
+
+				if (GUILayout.Button("Open Folder...", EditorStyles.miniButton))
+				{
+					ProjectExplorer.Open(fpath + "/package.json");
+				}
+			}
+
+		}
+	}
+}
---
+++ Assets/Source/Tools/Smidgenomics.PackageExport/Export/PackageExporter.cs
@@ -0,0 +1,123 @@
+﻿// Smidgens @ github
+
+namespace Smidgenomics.Tools.Packages
+{
+	using UnityEngine;
+	using UnityEditor;
+	using System;
+
+	/// <summary>
+	/// Package export window
+	/// </summary>
+	internal class PackageExporter : EditorWindow
+	{
+		public static class WLabel
+		{
+			public const string W_TITLE = "Export Package";
+			public const string EXPORT = "Export";
+			public const string VIEW = "View";
+			public const string REFRESH = "Refresh";
+			public const string PRESET_LIST_HEADER = "Export Presets";
+			public const string NO_PRESETS_MSG = "No Presets Found under '$PATH'";
+		}
+
+		public static void Open()
+		{
+			var w = GetWindow<PackageExporter>(typeof(SceneView));
+			w.titleContent.text = WLabel.W_TITLE;
+			w.Show();
+		}
+
+		// presets found in project
+		private ExportProfile[] _profiles = { };
+
+		// find query for asset database (cached due to interpolation)
+		private readonly string _PRESET_FIND_QUERY = $"t:{typeof(ExportProfile).Name}";
+
+		// folders to load export presets from
+		private readonly string[] _PRESET_FOLDERS =
+		{
+			CFG.AssetPath.EXPORT_PROFILES,
+		};
+
+		private readonly Lazy<GUIStyle> _HeaderLabel = new Lazy<GUIStyle>(() =>
+		{
+			var s = new GUIStyle(EditorStyles.boldLabel);
+			s.fontSize = 14;
+			s.normal.textColor = Color.white;
+			return s;
+		});
+
+		private void OnGUI()
+		{
+			GUILayout.Space(10f);
+			DrawProfileList();
+		}
+
+		private void OnEnable()
+		{
+			RefreshList();
+		}
+
+		// export preset (after gui update)
+		private void RunExport(ExportProfile p) => EditorApplication.delayCall += () => p.Export();
+
+		/// <summary>
+		/// Find profiles in project
+		/// </summary>
+		private void RefreshList()
+		{
+			var guids = AssetDatabase.FindAssets(_PRESET_FIND_QUERY, _PRESET_FOLDERS);
+			_profiles = new ExportProfile[guids.Length];
+			for(var i = 0; i < guids.Length; i++)
+			{
+				var path = AssetDatabase.GUIDToAssetPath(guids[i]);
+				_profiles[i] = AssetDatabase.LoadAssetAtPath<ExportProfile>(path);
+			}
+		}
+
+		/// <summary>
+		/// Draw list of export profiles
+		/// </summary>
+		private void DrawProfileList()
+		{
+
+			// [header] <flex> [refresh btn]
+			using (new GUILayout.HorizontalScope())
+			{
+				EditorGUILayout.LabelField(WLabel.PRESET_LIST_HEADER, _HeaderLabel.Value);
+
+				GUILayout.FlexibleSpace();
+
+				// refresh presets
+				if (GUILayout.Button(WLabel.REFRESH)) { RefreshList(); }
+			}
+
+			if (_profiles.Length == 0)
+			{
+				var msg = WLabel.NO_PRESETS_MSG.Replace("$PATH", CFG.AssetPath.EXPORT_PROFILES);
+				EditorGUILayout.HelpBox(msg, MessageType.Info);
+			}
+
+			foreach (var p in _profiles)
+			{
+				if (!p) { continue; } // maybe preset got deleted?
+
+				var profile = p; // loop scope thingy
+
+				// [name] <flex> [view btn][export btn]
+				using (new EditorGUILayout.HorizontalScope(GUI.skin.box))
+				{
+					EditorGUILayout.LabelField(p.name, EditorStyles.whiteLargeLabel);
+
+					// flex
+					GUILayout.FlexibleSpace();
+					// view btn
+					if (GUILayout.Button(WLabel.VIEW)) { Selection.activeObject = profile; }
+					// export btn
+					if (GUILayout.Button(WLabel.EXPORT)) { RunExport(profile); }
+				}
+			}
+		}
+	}
+}
